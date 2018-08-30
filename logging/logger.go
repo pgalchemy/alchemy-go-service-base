05a4f58b2d19ba0c8b374b32e4922dd4a44b4103
Soumik Paul <>
2018-08-30 13:08:41 +0530

    changed pg alchemy to pg suite

--- logging/logger.go
+++ logging/logger.go
@@ -7,7 +7,7 @@ import (
 	"time"
 
 	"github.com/gin-gonic/gin"
-	"github.com/pgalchemy/alchemy-go-service-base/scope"
+	"github.com/pgsuite/alchemy-go-service-base/scope"
 	"github.com/sirupsen/logrus"
 )
 
